19c176fc00424a7b91cd9399571c28d3bbd3da5c
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-03-20 13:32:41 -0300

    US14 - Criação do menu
    Signed-off-by: Guilherme Siqueira <guilhersiqueira@gmail.com>

---
+++ Reabilitação-Motora/Assets/Scripts/Fluxo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fluxo : MonoBehaviour {
+
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+	}
+
+	public void NotImplemented() {
+		SceneManager.LoadScene ("NotImplemented");
+	}
+
+	public void BackToMenu() {
+		SceneManager.LoadScene ("Menu");
+	}
+}
