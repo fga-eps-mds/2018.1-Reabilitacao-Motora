5287c238270bf6a3c55ff0aa1820c3945ddac9d4
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-03-26 21:52:02 -0300

    Extraindo texturas e acrescentando novo movimento associado à tecla S.
    Signed-off-by: Djorkaeff Alexandre <djorkaeff.unb@gmail.com>

---
+++ Reabilitação-Motora/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** 
+ *  Classe de animação do personagem.
+ */
+public class CharacterAnimation : MonoBehaviour 
+{
+	private Animator anim;
+	/** 
+     * Inicia o componente de animação do personagem.
+     */
+	void Start () 
+	{
+		anim = this.GetComponent<Animator>();
+	}
+		
+	/** 
+     *  Anima o personagem conforme pressionamento de teclas.
+     */
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.W)) 
+		{
+			anim.SetBool("Pointing", true);
+		}
+		if(Input.GetKeyUp(KeyCode.W)) 
+		{
+			anim.SetBool("Pointing", false);
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			anim.SetBool ("Arm Gesture", true);
+		}
+		if (Input.GetKeyUp (KeyCode.S)) 
+		{
+			anim.SetBool ("Arm Gesture", false);
+		}
+	}
+}
