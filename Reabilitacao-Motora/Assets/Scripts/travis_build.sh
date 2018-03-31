926f3fdc8ad9342222ee232c20609705a7f67fb0
Victor Moura <victor_cmoura@hotmail.com>
2018-03-31 00:02:51 -0300

    Corrigindo path de build do travis e sintaxe do script

--- Reabilitacao-Motora/Assets/Scripts/travis_build.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_build.sh
@@ -4,43 +4,43 @@ project="Reabilitacao-Motora"
 echo "Initializing Build Script for $project"
 echo "========================================"
 
-echo "The current path is $(pwd)"
+echo "The current path is $(pwd)/$project"
 echo "========================================"
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/$project/unity.log \
+  -projectPath $(pwd)/$project \
+  -buildWindowsPlayer "$(pwd)/$project/Build/windows/$project.exe" \
   -quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/$project/unity.log \
+  -projectPath $(pwd)/$project \
+  -buildOSXUniversalPlayer "$(pwd)/$project/Build/osx/$project.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/$project/unity.log \
+  -projectPath $(pwd)/$project \
+  -buildLinuxUniversalPlayer "$(pwd)/$project/Build/linux/$project.exe" \
   -quit
 
 echo 'Logs from build'
-cat $(pwd)/unity.log
+cat $(pwd)/$project/unity.log
 
 
 echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+zip -r $(pwd)/$project/Build/linux.zip $(pwd)/$project/Build/linux/
+zip -r $(pwd)/$project/Build/mac.zip $(pwd)/$project/Build/osx/
+zip -r $(pwd)/$project/Build/windows.zip $(pwd)/$project/Build/windows/
