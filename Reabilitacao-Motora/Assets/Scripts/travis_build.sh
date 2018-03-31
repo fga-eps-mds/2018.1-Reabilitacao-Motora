64d14a3094f5733dae7841676791475088c4e010
Victor Moura <victor_cmoura@hotmail.com>
2018-03-31 00:58:47 -0300

    Criando script para seleção de scene na build via linha de comando

--- Reabilitacao-Motora/Assets/Scripts/travis_build.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_build.sh
@@ -4,43 +4,63 @@ project="Reabilitacao-Motora"
 echo "Initializing Build Script for $project"
 echo "========================================"
 
-echo "The current path is $(pwd)/$project"
+echo "Opening $project in order to update meta files"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-force-free \
+-silent-crashes \
+-logFile $(pwd)/$project/unity.log \
+-projectPath $(pwd)/$project \
+-executeMethod TravisBuildSceneSelector.Perform \
+-quit
 echo "========================================"
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/$project/unity.log \
-  -projectPath $(pwd)/$project \
-  -buildWindowsPlayer "$(pwd)/$project/Build/windows/$project.exe" \
-  -quit
-
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/$project/unity.log \
-  -projectPath $(pwd)/$project \
-  -buildOSXUniversalPlayer "$(pwd)/$project/Build/osx/$project.app" \
-  -quit
-
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/$project/unity.log \
-  -projectPath $(pwd)/$project \
-  -buildLinuxUniversalPlayer "$(pwd)/$project/Build/linux/$project.exe" \
-  -quit
 
 echo 'Logs from build'
 cat $(pwd)/$project/unity.log
 
+# echo "Attempting to build $project for Windows"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# -force-free \
+# -batchmode \
+# -silent-crashes \
+# -logFile $(pwd)/$project/unity.log \
+# -projectPath $(pwd)/$project \
+# -executeMethod TravisBuildSceneSelector.Perform \
+# -buildWindowsPlayer "$(pwd)/$project/Build/windows/$project.exe" \
+# -quit
 
-echo 'Attempting to zip builds'
-zip -r $(pwd)/$project/Build/linux.zip $(pwd)/$project/Build/linux/
-zip -r $(pwd)/$project/Build/mac.zip $(pwd)/$project/Build/osx/
-zip -r $(pwd)/$project/Build/windows.zip $(pwd)/$project/Build/windows/
+# echo 'Logs from build'
+# cat $(pwd)/$project/unity.log
+
+# echo "Attempting to build $project for OS X"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# -force-free \
+# -batchmode \
+# -silent-crashes \
+# -logFile $(pwd)/$project/unity.log \
+# -projectPath $(pwd)/$project \
+# -executeMethod TravisBuildSceneSelector.Perform \
+# -buildOSXUniversalPlayer "$(pwd)/$project/Build/osx/$project.app" \
+# -quit
+
+# echo 'Logs from build'
+# cat $(pwd)/$project/unity.log
+
+# echo "Attempting to build $project for Linux"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# -force-free \
+# -batchmode \
+# -silent-crashes \
+# -logFile $(pwd)/$project/unity.log \
+# -projectPath $(pwd)/$project \
+# -executeMethod TravisBuildSceneSelector.Perform \
+# -buildLinuxUniversalPlayer "$(pwd)/$project/Build/linux/$project.exe" \
+# -quit
+
+# echo 'Logs from build'
+# cat $(pwd)/$project/unity.log
+
+
+# echo 'Attempting to zip builds'
+# zip -r $(pwd)/$project/Build/linux.zip $(pwd)/$project/Build/linux/
+# zip -r $(pwd)/$project/Build/mac.zip $(pwd)/$project/Build/osx/
+# zip -r $(pwd)/$project/Build/windows.zip $(pwd)/$project/Build/windows/
