a08f664849710724196535d0f5371fdc3511f134
Guilherme de Lyra <guilyra12@gmail.com>
2018-04-04 14:19:09 +0000

    Adaptando script fisico para SQLITE
    Signed-off-by: Joao Lucas <joao.lucas.ssr@gmail.com>

--- Banco_de_Dados/fisico.sql
+++ Banco_de_Dados/fisico.sql
@@ -4,76 +4,71 @@ USE fisiotech;
 
 
 CREATE TABLE IF NOT EXISTS PESSOA (
-	idPessoa BIGINT not null AUTO_INCREMENT,
+	idPessoa INTEGER PRIMARY KEY AUTOINCREMENT,
 	nomePessoa VARCHAR (30) not null,
 	sexo CHAR (1) not null,
-	dataNascimento DATE not null, 
-	constraint PESSOA_PK primary key (idPessoa)
-) ENGINE = innoDB;
+	dataNascimento DATE not null
+);
 
 
 CREATE TABLE IF NOT EXISTS TELEFONE (
-	idPessoa BIGINT not null,
-	telefone BIGINT not null,
-	constraint TELEFONE_PK primary key (idPessoa, telefone)
-) ENGINE = innoDB;
+	idPessoa INTEGER not null,
+	telefone INTEGER not null,
+	primary key (idPessoa, telefone),
+	foreign key (idPessoa) references PESSOA (idPessoa)
+);
 
 
 CREATE TABLE IF NOT EXISTS FISIOTERAPEUTA (
-	idFisioterapeuta BIGINT not null AUTO_INCREMENT,
-	idPessoa BIGINT not null,
+	idFisioterapeuta INTEGER PRIMARY KEY AUTOINCREMENT,
+	idPessoa INTEGER not null,
 	regiao VARCHAR (2),
-	crefito BIGINT,
-	constraint FISIOTERAPEUTA_PK primary key (idFisioterapeuta),
-	constraint FISIOTERAPEUTA_PESSOA_FK foreign key (idPessoa) references PESSOA (idPessoa)
-) ENGINE = innoDB;
+	crefito INTEGER,
+	foreign key (idPessoa) references PESSOA (idPessoa)
+);
 
 
 CREATE TABLE IF NOT EXISTS PACIENTE (
-	idPaciente BIGINT not null AUTO_INCREMENT,
-	idPessoa BIGINT not null,
+	idPaciente INTEGER PRIMARY KEY AUTOINCREMENT,
+	idPessoa INTEGER not null,
 	observacoes VARCHAR (300) not null,
-	constraint PACIENTE_PK primary key (idPaciente),
-	constraint PACIENTE_PESSOA_FK foreign key (idPessoa) references PESSOA (idPessoa)
-) ENGINE = innoDB;
+	foreign key (idPessoa) references PESSOA (idPessoa)
+);
 
 
 CREATE TABLE IF NOT EXISTS MUSCULO (
-	idMusculo BIGINT not null AUTO_INCREMENT,
-	nomeMusculo VARCHAR (20) not null,
-	constraint MUSCULO_PK primary key (idMusculo)
-) ENGINE = innoDB;
+	idMusculo INTEGER PRIMARY KEY AUTOINCREMENT,
+	nomeMusculo VARCHAR (20) not null
+);
 
 
 CREATE TABLE IF NOT EXISTS MOVIMENTO (
-	idMovimento BIGINT not null AUTO_INCREMENT,
-	idMovimentoPai BIGINT,
-	idPessoa BIGINT not null,
+	idMovimento INTEGER PRIMARY KEY AUTOINCREMENT,
+	idMovimentoPai INTEGER,
+	idPessoa INTEGER not null,
 	nomeMovimento VARCHAR (50),
 	graficoResultado VARCHAR (256),
 	movimentoExecutor VARCHAR (256),
 	rotuloMovimento VARCHAR (256),
-	constraint MOVIMENTO_PK primary key (idMovimento),
-	constraint MOVIMENTO_PAI_FK foreign key (idMovimentoPai) references MOVIMENTO (idMovimento),
-	constraint MOVIMENTO_PESSOA_FK foreign key (idPessoa) references PESSOA (idPessoa)
-) ENGINE = innoDB;
+	foreign key (idMovimentoPai) references MOVIMENTO (idMovimento),
+	foreign key (idPessoa) references PESSOA (idPessoa)
+);
 
 
 CREATE TABLE IF NOT EXISTS MOVIMENTOMUSCULO (
-	idMusculo BIGINT not null,
-	idMovimento BIGINT not null,
-	constraint MOVIMENTOMUSCULO_PK primary key (idMusculo, idMovimento),
-	constraint MOVIMENTOMUSCULO_MOVIMENTO_FK foreign key (idMovimento) references MOVIMENTO (idMovimento),
-	constraint MOVIMENTOMUSCULO_MUSCULO_FK foreign key (idMusculo) references MUSCULO (idMusculo)
-) ENGINE = innoDB;
+	idMusculo INTEGER not null,
+	idMovimento INTEGER not null, 
+	primary key (idMusculo, idMovimento),
+	foreign key (idMovimento) references MOVIMENTO (idMovimento),
+	foreign key (idMusculo) references MUSCULO (idMusculo)
+);
 
 
 CREATE TABLE IF NOT EXISTS SESSAO (
-	idSessao BIGINT not null AUTO_INCREMENT,
-	idFisioterapeuta BIGINT not null,
-	idPaciente BIGINT not null,
+	idSessao INTEGER PRIMARY KEY AUTOINCREMENT,
+	idFisioterapeuta INTEGER not null,
+	idPaciente INTEGER not null,
 	dataSessao DATE not null,
-	constraint SESSAO_PK primary key (idSessao),
-	constraint SESSAO_PACIENTE_FK foreign key (idPaciente) references PACIENTE (idPaciente),
-	constraint SESSAO_FISIOTERAPEUTA_FK foreign key (idFisioterapeuta) references FISIOTERAPEUTA (idFisioterapeuta)
-) ENGINE = innoDB;
+	foreign key (idPaciente) references PACIENTE (idPaciente),
+	foreign key (idFisioterapeuta) references FISIOTERAPEUTA (idFisioterapeuta)
+);
