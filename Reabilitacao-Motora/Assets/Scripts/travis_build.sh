d2f63492bc21b30cf397e10ffd181b6423e6632c
Victor Moura <victor_cmoura@hotmail.com>
2018-04-18 09:26:24 -0300

    Removendo linhas desnecessarias

--- Reabilitacao-Motora/Assets/Scripts/travis_build.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_build.sh
@@ -19,36 +19,6 @@ echo "========================================"
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile /dev/stdout -projectPath $(pwd) -buildWindowsPlayer "Build/windows/$project.exe" -quit
 echo "========================================"
-# echo "Attempting to build $project for OS X"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -nographics \
-# -batchmode \
-# -serial $UNITY_KEY \
-# -username $UNITY_LOGIN \
-# -password $UNITY_PASSWORD \
-# -logFile $(pwd)/$project/unity.log \
-# -projectPath $(pwd)/$project \
-# -buildOSXUniversalPlayer "$(pwd)/$project/Build/osx/$project.app" \
-# -quit
-
-# echo 'Logs from build'
-# cat $(pwd)/$project/unity.log
-
-# echo "Attempting to build $project for Linux"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -nographics \
-# -batchmode \
-# -serial $UNITY_KEY \
-# -username $UNITY_LOGIN \
-# -password $UNITY_PASSWORD \
-# -logFile $(pwd)/$project/unity.log \
-# -projectPath $(pwd)/$project \
-# -buildLinuxUniversalPlayer "$(pwd)/$project/Build/linux/$project.exe" \
-# -quit
-
-# echo 'Logs from build'
-# cat $(pwd)/$project/unity.log
-
 echo "Opening in order to deactivate Unity"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -returnlicense -logFile /dev/stdout -quit
 echo "========================================"
