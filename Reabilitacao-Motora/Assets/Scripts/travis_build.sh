fccf18fc842075c667ec314ad2ba6e39e62eab0e
Victor Moura <victor_cmoura@hotmail.com>
2018-04-18 08:53:11 -0300

    Ativando builds para Linux e Windows

--- Reabilitacao-Motora/Assets/Scripts/travis_build.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_build.sh
@@ -1,45 +1,24 @@
 #! /bin/sh
 project="Reabilitacao-Motora"
-
-echo "Initializing Build Script for $project"
 echo "========================================"
-
+echo "Initializing Build Script for $project"
 cd Reabilitacao-Motora
+echo "========================================"
+echo "Current folder contains:"
 ls
-
-# echo "Opening in order to load Unity"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -batchmode \
-# -nographics \
-# -projectPath $(pwd)/$project \
-# -logFile $(pwd)/$project/unity.log \
-# -quit
-# echo "========================================"
-
-# echo 'Logs from opening'
-# cat $(pwd)/$project/unity.log
-# echo "========================================"
-
-# echo "Opening in order to activate Unity"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -batchmode \
-# -nographics \
-# -serial $UNITY_KEY \
-# -username $UNITY_LOGIN \
-# -password $UNITY_PASSWORD \
-# -logFile $(pwd)/$project/unity.log \
-# -quit
-# echo "========================================"
-
-# echo 'Logs from activation'
-# cat $(pwd)/$project/unity.log
-
+echo "========================================"
 echo "Attempting to activate Unity"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -serial $UNITY_KEY -username $UNITY_LOGIN -password $UNITY_PASSWORD -logFile /dev/stdout -quit
-
+echo "========================================"
 echo "Attempting to build $project for OSX"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile /dev/stdout -projectPath $(pwd) -buildOSXUniversalPlayer "Build/osx/$project.exe" -quit
-
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile /dev/stdout -projectPath $(pwd) -buildOSXUniversalPlayer "Build/osx/$project.app" -quit
+echo "========================================"
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile /dev/stdout -projectPath $(pwd) -buildLinuxUniversalPlayer "Build/linux/$project.exe" -quit
+echo "========================================"
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile /dev/stdout -projectPath $(pwd) -buildWindowsPlayer "Build/windows/$project.exe" -quit
+echo "========================================"
 # echo "Attempting to build $project for OS X"
 # /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 # -nographics \
@@ -71,13 +50,10 @@ echo "Attempting to build $project for OSX"
 # cat $(pwd)/$project/unity.log
 
 echo "Opening in order to deactivate Unity"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -returnlicense -logFile unity.log -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -returnlicense -logFile /dev/stdout -quit
 echo "========================================"
 
-echo 'Logs from deactivation'
-cat unity.log
-
 echo 'Attempting to zip builds'
-# zip -r $(pwd)/$project/Build/linux.zip $(pwd)/$project/Build/linux/
+zip -r Build/linux.zip Build/linux/
 zip -r Build/mac.zip Build/osx/
-# zip -r Build/windows.zip /Build/windows/
+zip -r Build/windows.zip Build/windows/
