58c7b210074cd4754e84178668c7241b53b077db
Joao Lucas <joao.lucas.ssr@gmail.com>
2018-05-06 18:09:09 -0300

    Adicionando o PopUp de deletar
    Signed-off-by: Guilherme de Lyra <guilyra12@gmail.com>

---
+++ Reabilitacao-Motora/Assets/PopUpSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpSpawner : MonoBehaviour {
+
+    public GameObject PopUpPrefab;
+
+    public void Spawner()
+    {
+        GameObject jo = Instantiate(PopUpPrefab, transform);
+    }
+}
