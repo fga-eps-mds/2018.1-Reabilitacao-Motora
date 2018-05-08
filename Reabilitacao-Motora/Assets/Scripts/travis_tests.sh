2d4b55df19030b1e74c82ea6eb7537b35f457513
Victor Moura <victor_cmoura@hotmail.com>
2018-05-08 12:16:56 -0300

    Adicionando stages de build - master_stage: Versao de Release - homolog_stage: Versao de Homologacao - development_stage: Versao de Desenvolvimento

--- Reabilitacao-Motora/Assets/Scripts/travis_tests.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_tests.sh
@@ -1,67 +1,15 @@
-#! /bin/sh
-project="Reabilitacao-Motora"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile /dev/stdout -runEditorTests -projectPath $(pwd) -testPlatform playmode -editorTestsResultFile test.xml
+ rc0=$?
 
-echo "Initializing Build Script for $project"
-echo "========================================"
+echo " Untiy test Logs"
+cat $(pwd)test.xml
 
-cd Reabilitacao-Motora
-
-echo "Opening $project in order to update meta files"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -logFile unity.log -projectPath . -runEditorTests -editorTestsResultFile test.xml rc0=$?
-
-echo "========================================"
-echo "Unity logs"
-cat unity.log
-echo "========================================"
-echo "Test logs"
-cat test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
-# echo "Attempting to build $project for Windows"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -force-free \
-# -batchmode \
-# -silent-crashes \
-# -logFile $(pwd)/$project/unity.log \
-# -projectPath $(pwd)/$project \
-# -executeMethod TravisBuildSceneSelector.Perform \
-# -buildWindowsPlayer "$(pwd)/$project/Build/windows/$project.exe" \
-# -quit
+#CASO NÃO FUNCUONE TENTAR USAR AS CATEGORIAS
+# -editorTestsCategories ADICIONAR A CATEGORIA AQUI \
 
-# echo 'Logs from build'
-# cat $(pwd)/$project/unity.log
+# -testFilter "<projectPath>/Library/ScriptAssemblies/Assembly-CSharp.dll" talvez tenha que adicionar caso ocorra este erro  (One or more child tests had errors" error in the Unity)
 
-# echo "Attempting to build $project for OS X"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -force-free \
-# -batchmode \
-# -silent-crashes \
-# -logFile $(pwd)/$project/unity.log \
-# -projectPath $(pwd)/$project \
-# -executeMethod TravisBuildSceneSelector.Perform \
-# -buildOSXUniversalPlayer "$(pwd)/$project/Build/osx/$project.app" \
-# -quit
-
-# echo 'Logs from build'
-# cat $(pwd)/$project/unity.log
-
-# echo "Attempting to build $project for Linux"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# -force-free \
-# -batchmode \
-# -silent-crashes \
-# -logFile $(pwd)/$project/unity.log \
-# -projectPath $(pwd)/$project \
-# -executeMethod TravisBuildSceneSelector.Perform \
-# -buildLinuxUniversalPlayer "$(pwd)/$project/Build/linux/$project.exe" \
-# -quit
-
-# echo 'Logs from build'
-# cat $(pwd)/$project/unity.log
-
-
-# echo 'Attempting to zip builds'
-# zip -r $(pwd)/$project/Build/linux.zip $(pwd)/$project/Build/linux/
-# zip -r $(pwd)/$project/Build/mac.zip $(pwd)/$project/Build/osx/
-# zip -r $(pwd)/$project/Build/windows.zip $(pwd)/$project/Build/windows/
+#Opção aparentemente viável -executeMethod <ClassName.MethodName> CASO OS TESTES ESTEJAM NA CLASSE TEST PODEM SER EXECUTADOS AQUI (Só funciona com métodos estáticos)
