f6cab4f1109deed39b6b0f4abf561d64ddcc3d70
Victor Moura <victor_cmoura@hotmail.com>
2018-05-12 12:10:19 -0300

    Fixing upload file name

--- Reabilitacao-Motora/Assets/Scripts/travis_deploy_master.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_deploy_master.sh
@@ -12,5 +12,5 @@ echo 'Attempting to upload Linux build'
 curl -X POST -i -F file_object=@linux.zip ec2-54-233-228-194.sa-east-1.compute.amazonaws.com:3000/file_objects/master/Linux
 echo "========================================"
 echo 'Attempting to upload OSX build'
-curl -X POST -i -F file_object=@osx.zip ec2-54-233-228-194.sa-east-1.compute.amazonaws.com:3000/file_objects/master/OSX
+curl -X POST -i -F file_object=@mac.zip ec2-54-233-228-194.sa-east-1.compute.amazonaws.com:3000/file_objects/master/OSX
 echo "========================================"
