5645303fdd3d363b9c50b09559b8d1f1963b2481
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-06-03 21:59:57 -0300

    Corrigindo erro no script de troca de personagem

--- Reabilitacao-Motora/Assets/Scripts/Characters/AvatarChange.cs
+++ Reabilitacao-Motora/Assets/Scripts/Characters/AvatarChange.cs
@@ -21,27 +21,22 @@ public class AvatarChange : MonoBehaviour
         if (GlobalController.choiceAvatar==1)
         {
             man1Avatar.SetActive(true);
-            man1Avatar.GetComponent<Renderer>().enabled = true;
         }
         else if (GlobalController.choiceAvatar==2)
         {
             man2Avatar.SetActive(true);
-            man2Avatar.GetComponent<Renderer>().enabled = true;
         }
         else if (GlobalController.choiceAvatar==3)
         {
             man3Avatar.SetActive(true);
-            man3Avatar.GetComponent<Renderer>().enabled = true;
         }
         else if (GlobalController.choiceAvatar==4)
         {
             woman1Avatar.SetActive(true);
-            woman1Avatar.GetComponent<Renderer>().enabled = true;
         }
         else if (GlobalController.choiceAvatar==5)
         {
             woman2Avatar.SetActive(true);
-            woman2Avatar.GetComponent<Renderer>().enabled = true;
         }
     }
 }
