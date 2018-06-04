a117a3ee77b56383ecec897c677cde9eaeac01fe
Victor Moura <victor_cmoura@hotmail.com>
2018-06-04 10:11:39 -0300

    Logs de teste sendo mostrados em sua integridade

--- Reabilitacao-Motora/Assets/Scripts/travis_tests.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_tests.sh
@@ -13,7 +13,8 @@ echo "${TEXTOAMARELO} Iniciando Script de PlayTest ${NORMAL}"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -runTests -projectPath $(pwd) -testPlatform playmode -logFile /dev/stdout -testResults /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/playTest.xml
 rc0=$?
 echo " ${TEXTOAMARELO} Exibindo Log do Script de PlayTest ${NORMAL}"
-python /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/Assets/Scripts/playTest_log_converter.py
+#python /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/Assets/Scripts/playTest_log_converter.py
+cat /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/playTest.xml
 echo "${TEXTOVERDE} ======================================= FIM DOS LOGS DO PLAY TEST ======================================= ${NORMAL}"
 if [ $rc0 -ne 0 ]; then { echo " PlayTest Falhando -- Consultar Log de Teste -- "; exit $rc0; } fi
 
@@ -21,6 +22,7 @@ echo "${TEXTOAMARELO} Iniciando Script de EditorTest ${NORMAL}"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode  -projectPath $(pwd) -runEditorTests -logFile /dev/stdout -editorTestsResultFile /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/editorTest.xml
 rc1=$?
 echo " ${TEXTOAMARELO} Exibindo Log do Script de EditorsTests ${NORMAL}"
-python /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/Assets/Scripts/editorTest_log_converter.py
+#python /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/Assets/Scripts/editorTest_log_converter.py
+cat /Users/travis/build/fga-gpp-mds/2018.1-Reabilitacao-Motora/Reabilitacao-Motora/editorTest.xml
 echo "${TEXTOVERDE} ======================================= FIM DO SCRIPT EDITORS TEST ======================================= ${NORMAL}"
 if [ $rc1 -ne 0 ]; then { echo " Editors Tests Falhando -- Consultar Log de Teste -- "; exit $rc0; } fi
