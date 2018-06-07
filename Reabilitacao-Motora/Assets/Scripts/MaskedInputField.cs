1da65d2413fbc328337541dcd9b0a2dada3c7cd3
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-06-07 17:44:42 -0300

    Criando script de máscara de data de nascimento

---
+++ Reabilitacao-Motora/Assets/Scripts/MaskedInputField.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MaskedInputField : MonoBehaviour
+{
+    public InputField mainInputField;
+    public Text outputText;
+
+    public void Start()
+    {
+        //Adds a listener to the main input field and invokes a method when the value changes.
+        mainInputField.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the text field changes.
+    public void ValueChangeCheck()
+    {
+        int index = 0 ;
+        string format = "##/##/####" ;
+        string output = format;
+        string input;
+
+        input = mainInputField.text;
+        
+        for( int i = 0 ; i < input.Length ; ++i )
+        {
+            index = output.IndexOf("#");
+            if (index < 0)
+            {
+                break ;
+            }
+            if (index == 0)
+            {
+                output = input[i] + output.Substring ( 1 );
+            }
+            else if (index == output.Length - 1)
+            {
+                output = output.Substring ( 0, index ) + input[i] ;
+            }
+            else
+            {
+                output = output.Substring ( 0, index ) + input[i] + output.Substring ( index + 1 );
+            }
+        }
+
+        if ( index >= 0 )
+        {
+            output = output.Substring( 0, index + 1 );
+        }
+
+        if(output=="#") 
+        {
+            outputText = "";
+        }
+        else
+        {
+            outputText = output;
+        }
+
+        Debug.Log( output ); // Will output 95/3
+    }
+}
