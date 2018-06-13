bdbd3cc864e840521c9bb082dca95813188eabca
Guilherme de Lyra <guilyra12@gmail.com>
2018-06-13 04:14:32 +0000

    Maioria das scenes feitas, script ainda com problema.

--- Reabilitacao-Motora/Assets/Scripts/Graphs/Label/DrawAreaLabel.cs
+++ Reabilitacao-Motora/Assets/Scripts/Graphs/Label/DrawAreaLabel.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using pontosrotulofisioterapeuta;
+using pontosrotulopaciente;
+using UnityEngine.EventSystems;
 
 public class DrawAreaLabel : MonoBehaviour
 {
 	[SerializeField]
 	protected Camera Cam;
 	[SerializeField]
-	protected GameObject labelPrefab, popUp;
+	protected GameObject labelPrefab, popUp, canvas;
 	[SerializeField]
 	protected Transform originGrafico;
 	[SerializeField]
@@ -16,92 +19,130 @@ public class DrawAreaLabel : MonoBehaviour
 	protected Image ImagemSelecao;
 
 	protected Vector3 PosMinMouse, PosMaxMouse, PosIniMouse, origin;
-	protected RaycastHit R;
+	// protected RaycastHit R;
+	bool patientOrPhysio;
 
 	public void Start ()
 	{
+		var currentscene = SceneManager.GetActiveScene().name;
+		if (currentscene.Substring(currentscene.Length-5, 4) == "ient")
+		{
+			patientOrPhysio = false;
+		}
+		else
+		{
+			patientOrPhysio = true;		
+		}
+
 		origin = Cam.WorldToScreenPoint(originGrafico.position);
+	}
+
+	public bool ValidClick (Vector2 pos)
+	{
+		if (pos.x >= origin.x && pos.y >= origin.y)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	public void Update ()
 	{	
-		if(Input.GetMouseButtonDown(0))
+		if (ValidClick(Input.mousePosition))
 		{
-			if (Input.mousePosition.x <= origin.x)
-			{
-				PosMinMouse = origin;
-				PosIniMouse = origin;
-			} 
-			else
+			if(Input.GetMouseButtonDown(1))
 			{
 				PosMinMouse = Input.mousePosition;
 				PosIniMouse = Input.mousePosition;
 			}
-		}
 
-		if(Input.GetMouseButton(0))
-		{
-			if (Input.mousePosition.x >= PosIniMouse.x)
+			if(Input.GetMouseButton(1))
 			{
-				PosMaxMouse.x = Input.mousePosition.x;   
+				if (Input.mousePosition.x >= PosIniMouse.x)
+				{
+					PosMaxMouse.x = Input.mousePosition.x;   
+				}
+				else if (Input.mousePosition.x >= origin.x && Input.mousePosition.x < PosIniMouse.x)
+				{
+					PosMaxMouse.x = PosIniMouse.x;
+					PosMinMouse.x = Input.mousePosition.x; 
+				}
+				else if (Input.mousePosition.x < origin.x)
+				{
+					PosMaxMouse.x = PosIniMouse.x;
+					PosMinMouse.x = origin.x; 
+				}
+				
+				var max = new Vector2(((PosMaxMouse.x - origin.x) / Screen.width), 1f);
+				var min = new Vector2(((PosMinMouse.x - origin.x) / Screen.width), 0f);
+
+				ImagemSelecao.rectTransform.anchorMax = max;
+				ImagemSelecao.rectTransform.anchorMin = min; 
 			}
-			else if (Input.mousePosition.x >= origin.x)
+
+			if(Input.GetMouseButtonUp(1))
 			{
-				PosMaxMouse.x = PosIniMouse.x;
-				PosMinMouse.x = Input.mousePosition.x; 
+				// Physics.Raycast(Cam.ScreenPointToRay(PosMinMouse), out R);
+				PosMinMouse = Cam.ScreenToWorldPoint(PosMinMouse);
+
+				// Physics.Raycast(Cam.ScreenPointToRay(PosMaxMouse), out R);
+				PosMaxMouse = Cam.ScreenToWorldPoint(PosMaxMouse);
+
+				popUp.SetActive(true);
+				DesactivateCanvas();
+
+				Debug.Log(PosMinMouse);
+				Debug.Log(PosMaxMouse);
+				ImagemSelecao.rectTransform.anchorMax = new Vector2(0,0);
+				ImagemSelecao.rectTransform.anchorMin = new Vector2(0,0);
 			}
-			else
-			{
-				PosMaxMouse.x = PosIniMouse.x;
-				PosMinMouse.x = origin.x; 
-			}
-			Debug.Log(PosMinMouse.x);
-			Debug.Log(origin.x);
-			Debug.Log(Screen.width);
-			Debug.Log((PosMinMouse.x - origin.x) / Screen.width);
-			var max = new Vector2(((PosMaxMouse.x - origin.x) / Screen.width), 1f);
-			var min = new Vector2(((PosMinMouse.x - origin.x) / Screen.width), 0f);
-			ImagemSelecao.rectTransform.anchorMax = max;
-			ImagemSelecao.rectTransform.anchorMin = min; 
-		}
-
-		if(Input.GetMouseButtonUp(0))
-		{
-			Physics.Raycast(Cam.ScreenPointToRay(PosMinMouse), out R);
-			PosMinMouse = R.point;
-
-			Physics.Raycast(Cam.ScreenPointToRay(PosMaxMouse), out R);
-			PosMaxMouse = R.point;
-
-			popUp.SetActive(true);
-			
-			ImagemSelecao.rectTransform.anchorMax = new Vector2(0,0);
-			ImagemSelecao.rectTransform.anchorMin = new Vector2(0,0);
 		}
 	}
 
-	public void displayGraph(string label, Vector2 val)
+	public void displayGraph ()
 	{
 		GameObject go = Instantiate (labelPrefab) as GameObject;
 
 		go.transform.localPosition = new Vector3 (0f, 0f, 0f);
-		go.transform.SetParent (transform, false);
+		go.transform.parent = gameObject.transform.parent;
 
 		var scriptInitial = go.GetComponentInChildren<SetInitialX>();
 		var scriptFinal = go.GetComponentInChildren<SetFinalX>();
 		var labelName = go.GetComponentInChildren<TextMesh>();
-		var dbPrfObject = go.GetComponentInChildren<SetLabelPhysio>();
-
-		scriptInitial.InitialX = val.x;
-		scriptFinal.FinalX = val.y;
+		
+		scriptInitial.InitialX = PosMinMouse.x;
+		scriptFinal.FinalX = PosMaxMouse.x;
 
 		scriptInitial.Set();
 		scriptFinal.Set();
 
-		labelName.text = label;
+		labelName.text = textField.text;
 
-		PontosRotuloFisioterapeuta.Insert(GlobalController.instance.movement.idMovimento, label, val.x, val.y);
+		if (patientOrPhysio)
+		{
+			PontosRotuloFisioterapeuta.Insert(GlobalController.instance.movement.idMovimento, textField.text, PosMinMouse.x, PosMaxMouse.x);
+			var dbPrfObject = go.GetComponentInChildren<SetLabelPhysio>();
+			dbPrfObject.Prf = PontosRotuloFisioterapeuta.GetLast();
+		}
+		else
+		{
+			PontosRotuloPaciente.Insert(GlobalController.instance.exercise.idExercicio, textField.text, PosMinMouse.x, PosMaxMouse.x);
+			var dbPrpObject = go.GetComponentInChildren<SetLabelPatient>();
+			dbPrpObject.Prp = PontosRotuloPaciente.GetLast();
+		}
 
-		dbPrfObject.Prf = PontosRotuloFisioterapeuta.GetLast();
+	}
+
+	public void DesactivateCanvas ()
+	{
+		canvas.SetActive(false);
+	}
+
+	public void ActivateCanvas ()
+	{
+		canvas.SetActive(true);
 	}
 }
