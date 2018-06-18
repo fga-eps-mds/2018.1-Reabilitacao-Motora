723e1b8313c00f8ca25ec1bebeef6026d983cc59
Victor Moura <victor_cmoura@hotmail.com>
2018-06-17 21:15:29 -0300

    Alinhando botoes com personagens na tela de selecionar char

---
+++ Reabilitacao-Motora/Assets/Scripts/Buttons/ClipButtonToObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClipButtonToObject : MonoBehaviour {
+
+	public Button objectButton;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Camera.main.WorldToScreenPoint(this.transform.position);
+		objectButton.transform.position = pos;
+	}
+}
