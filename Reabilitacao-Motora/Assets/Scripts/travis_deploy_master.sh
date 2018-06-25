7024d2dfd26cc0e58f3efe088e62bff3baad51dd
Victor Moura <victor_cmoura@hotmail.com>
2018-06-25 18:44:10 -0300

    Mudando link da amazon

--- Reabilitacao-Motora/Assets/Scripts/travis_deploy_master.sh
+++ Reabilitacao-Motora/Assets/Scripts/travis_deploy_master.sh
@@ -6,11 +6,11 @@ echo "Current folder contains:"
 ls
 echo "========================================"
 echo 'Attempting to upload Windows build'
-curl -X POST -i -F file_object=@windows.zip ec2-54-233-228-194.sa-east-1.compute.amazonaws.com:3000/file_objects/master/Windows
+curl -X POST -i -F file_object=@windows.zip ec2-18-231-174-28.sa-east-1.compute.amazonaws.com:3000/file_objects/master/Windows
 echo "========================================"
 echo 'Attempting to upload Linux build'
-curl -X POST -i -F file_object=@linux.zip ec2-54-233-228-194.sa-east-1.compute.amazonaws.com:3000/file_objects/master/Linux
+curl -X POST -i -F file_object=@linux.zip ec2-18-231-174-28.sa-east-1.compute.amazonaws.com:3000/file_objects/master/Linux
 echo "========================================"
 echo 'Attempting to upload OSX build'
-curl -X POST -i -F file_object=@mac.zip ec2-54-233-228-194.sa-east-1.compute.amazonaws.com:3000/file_objects/master/OSX
+curl -X POST -i -F file_object=@mac.zip ec2-18-231-174-28.sa-east-1.compute.amazonaws.com:3000/file_objects/master/OSX
 echo "========================================"
