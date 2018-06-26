6c34a9b5ee5c8e380b2692648d81a1af826c4b04
Guilherme de Lyra <guilyra12@gmail.com>
2018-06-26 09:43:07 +0000

    consertando bug de reproduçao de movimento do fisioterapeuta nas telas de reproduçao de exercicio

--- Reabilitacao-Motora/Assets/Scripts/Graphs/GenerateLineChart.cs
+++ Reabilitacao-Motora/Assets/Scripts/Graphs/GenerateLineChart.cs
@@ -16,7 +16,7 @@ public class GenerateLineChart : MonoBehaviour
 	protected Transform mao, ombro, cotovelo, braco;
 
 	[SerializeField]
-	protected GameObject popUpLabel;
+	protected GameObject popUpLabel, pers1, pers2, pers3, pers4, pers5;
 
 	[System.Serializable]
 	public class NecessaryJoints
@@ -48,6 +48,7 @@ public class GenerateLineChart : MonoBehaviour
 	private static readonly Color c2 = Color.red;
 	private static readonly Color c3 = Color.blue;
 
+	int choiceAvatar;
 	bool t;
 	bool drawed;
 
@@ -56,47 +57,57 @@ public class GenerateLineChart : MonoBehaviour
 	*/
 	public void LoadData(string[] lines)
 	{
+		bool got_sensor = false;
 		foreach (var line in lines)
 		{
-			var pair = line.Split(' ');
-			float x = (float.Parse(pair[0]));
-			current_time.Add(x);
+			if (!got_sensor)
+			{
+				Debug.Log(line);
+				choiceAvatar = int.Parse(line);
+				got_sensor = true;
+			}
+			else
+			{
+				var pair = line.Split(' ');
+				float x = (float.Parse(pair[0]));
+				current_time.Add(x);
 
-			float a = (float.Parse(pair[1]));
-			float b = (float.Parse(pair[2]));
-			float c = (float.Parse(pair[3]));
-			f_mao_pos.Add(new Vector3 (a, b, c));
-			a = (float.Parse(pair[4]));
-			b = (float.Parse(pair[5]));
-			c = (float.Parse(pair[6]));
-			f_mao_rot.Add(new Vector3 (a, b, c));
+				float a = (float.Parse(pair[1]));
+				float b = (float.Parse(pair[2]));
+				float c = (float.Parse(pair[3]));
+				f_mao_pos.Add(new Vector3 (a, b, c));
+				a = (float.Parse(pair[4]));
+				b = (float.Parse(pair[5]));
+				c = (float.Parse(pair[6]));
+				f_mao_rot.Add(new Vector3 (a, b, c));
 
-			a = (float.Parse(pair[7]));
-			b = (float.Parse(pair[8]));
-			c = (float.Parse(pair[9]));
-			f_cotovelo_pos.Add(new Vector3 (a, b, c));
-			a = (float.Parse(pair[10]));
-			b = (float.Parse(pair[11]));
-			c = (float.Parse(pair[12]));
-			f_cotovelo_rot.Add(new Vector3 (a, b, c));
+				a = (float.Parse(pair[7]));
+				b = (float.Parse(pair[8]));
+				c = (float.Parse(pair[9]));
+				f_cotovelo_pos.Add(new Vector3 (a, b, c));
+				a = (float.Parse(pair[10]));
+				b = (float.Parse(pair[11]));
+				c = (float.Parse(pair[12]));
+				f_cotovelo_rot.Add(new Vector3 (a, b, c));
 
-			a = (float.Parse(pair[13]));
-			b = (float.Parse(pair[14]));
-			c = (float.Parse(pair[15]));
-			f_ombro_pos.Add(new Vector3 (a, b, c));
-			a = (float.Parse(pair[16]));
-			b = (float.Parse(pair[17]));
-			c = (float.Parse(pair[18]));
-			f_ombro_rot.Add(new Vector3 (a, b, c));
+				a = (float.Parse(pair[13]));
+				b = (float.Parse(pair[14]));
+				c = (float.Parse(pair[15]));
+				f_ombro_pos.Add(new Vector3 (a, b, c));
+				a = (float.Parse(pair[16]));
+				b = (float.Parse(pair[17]));
+				c = (float.Parse(pair[18]));
+				f_ombro_rot.Add(new Vector3 (a, b, c));
 
-			a = (float.Parse(pair[19]));
-			b = (float.Parse(pair[20]));
-			c = (float.Parse(pair[21]));
-			f_braco_pos.Add(new Vector3 (a, b, c));
-			a = (float.Parse(pair[22]));
-			b = (float.Parse(pair[23]));
-			c = (float.Parse(pair[24]));
-			f_braco_rot.Add(new Vector3 (a, b, c));					
+				a = (float.Parse(pair[19]));
+				b = (float.Parse(pair[20]));
+				c = (float.Parse(pair[21]));
+				f_braco_pos.Add(new Vector3 (a, b, c));
+				a = (float.Parse(pair[22]));
+				b = (float.Parse(pair[23]));
+				c = (float.Parse(pair[24]));
+				f_braco_rot.Add(new Vector3 (a, b, c));
+			}
 		}
 	}
 	
@@ -121,35 +132,36 @@ public class GenerateLineChart : MonoBehaviour
 			f_braco_pos = new List<Vector3>();
 			f_braco_rot = new List<Vector3>();
 			
-			string file;
-
 			var currentscene = SceneManager.GetActiveScene().name;
 			var go = gameObject;
 			
 			if (currentscene == "GraphsMovimentoPhysio")
 			{
-				Assign(false);
-				file = Application.dataPath + "/Movimentos/" + GlobalController.instance.movement.pontosMovimento;
+				string file = Application.dataPath + "/Movimentos/" + GlobalController.instance.movement.pontosMovimento;
+				string[] p1 = System.IO.File.ReadAllLines(file);
+				LoadData (p1);
+				Assign();
 				GetMovementPoints.LoadLineRenderer(ref go, ref lineRenderer, c1, c2);
 			}
 			else
 			{
 				if (transform.root.gameObject.name == "Grafico Fisio")
 				{
-					Assign(true);
-					file = Application.dataPath + "/Movimentos/" + GlobalController.instance.movement.pontosMovimento;
+					string file = Application.dataPath + "/Movimentos/" + GlobalController.instance.movement.pontosMovimento;
+					string[] p1 = System.IO.File.ReadAllLines(file);
+					LoadData (p1);
+					Assign();
 					GetMovementPoints.LoadLineRenderer(ref go, ref lineRenderer, c1, c2);
 				}
 				else
 				{
-					Assign(false);
-					file = Application.dataPath + "/Exercicios/" + GlobalController.instance.exercise.pontosExercicio;
+					string file = Application.dataPath + "/Exercicios/" + GlobalController.instance.exercise.pontosExercicio;
+					string[] p1 = System.IO.File.ReadAllLines(file);
+					LoadData (p1);
+					Assign();
 					GetMovementPoints.LoadLineRenderer(ref go, ref lineRenderer, c1, c3);
 				}
 			}
-
-			string[] p1 = System.IO.File.ReadAllLines(file);
-			LoadData (p1);
 		}
 		else
 		{
@@ -158,54 +170,50 @@ public class GenerateLineChart : MonoBehaviour
 		
 	}
 
-	public void Assign (bool physio)
+	public void Assign ()
 	{
-		if (physio == true)
+		if (choiceAvatar == 1)
 		{
 			ombro = joints[0].ombroAux;
 			braco = joints[0].bracoAux;
 			cotovelo = joints[0].cotoveloAux;
 			mao = joints[0].maoAux;
+			pers1.SetActive(true);
 		}
-		else
+		else if (choiceAvatar == 2)
 		{
-			if (GlobalController.choiceAvatar == 1)
-			{
-				ombro = joints[0].ombroAux;
-				braco = joints[0].bracoAux;
-				cotovelo = joints[0].cotoveloAux;
-				mao = joints[0].maoAux;
-			}
-			else if (GlobalController.choiceAvatar == 2)
-			{
-				ombro = joints[1].ombroAux;
-				braco = joints[1].bracoAux;
-				cotovelo = joints[1].cotoveloAux;
-				mao = joints[1].maoAux;
-			}
-			else if (GlobalController.choiceAvatar == 3)
-			{
-				ombro = joints[2].ombroAux;
-				braco = joints[2].bracoAux;
-				cotovelo = joints[2].cotoveloAux;
-				mao = joints[2].maoAux;
-			}
-			else if (GlobalController.choiceAvatar == 4)
-			{
-				ombro = joints[3].ombroAux;
-				braco = joints[3].bracoAux;
-				cotovelo = joints[3].cotoveloAux;
-				mao = joints[3].maoAux;
-			}
-			else if (GlobalController.choiceAvatar == 5)
-			{
-				ombro = joints[4].ombroAux;
-				braco = joints[4].bracoAux;
-				cotovelo = joints[4].cotoveloAux;
-				mao = joints[4].maoAux;
-			}
+			ombro = joints[1].ombroAux;
+			braco = joints[1].bracoAux;
+			cotovelo = joints[1].cotoveloAux;
+			mao = joints[1].maoAux;
+			pers2.SetActive(true);
+		}
+		else if (choiceAvatar == 3)
+		{
+			ombro = joints[2].ombroAux;
+			braco = joints[2].bracoAux;
+			cotovelo = joints[2].cotoveloAux;
+			mao = joints[2].maoAux;
+			pers3.SetActive(true);
+		}
+		else if (choiceAvatar == 4)
+		{
+			ombro = joints[3].ombroAux;
+			braco = joints[3].bracoAux;
+			cotovelo = joints[3].cotoveloAux;
+			mao = joints[3].maoAux;
+			pers4.SetActive(true);
+		}
+		else if (choiceAvatar == 5)
+		{
+			ombro = joints[4].ombroAux;
+			braco = joints[4].bracoAux;
+			cotovelo = joints[4].cotoveloAux;
+			mao = joints[4].maoAux;
+			pers5.SetActive(true);
 		}
 	}
+
 	/**
 	* Descrever aqui o que esse método realiza.
 	*/
@@ -239,7 +247,7 @@ public class GenerateLineChart : MonoBehaviour
 			mao.localPosition = f_mao_pos[i];
 			mao.localEulerAngles = f_mao_rot[i];
 
-			yield return new WaitForSeconds(0.01f);        
+			yield return new WaitForSeconds(0.025f);        
 		} 
 	}
 
@@ -248,7 +256,7 @@ public class GenerateLineChart : MonoBehaviour
 		for (int j = 0; j < current_time.Count; ++j) 
 		{
 			GetMovementPoints.graphSpawner(transform, pointPrefab, mao, cotovelo, ombro, current_time[j], ref lineRenderer);
-			yield return new WaitForSeconds(0.01f);
+			yield return new WaitForSeconds(0.025f);
 		}
 	}
 }
